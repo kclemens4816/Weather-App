6a1686c80f4e64c4e405350147272ffa5e893d16
kclemens4816 <kclemens4816@gmail.com>
2025-05-23 13:29:47 -0500

    injected the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -68,7 +68,33 @@ function changeCity(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="forecast-day">
+          <div class="forecast-date">${day}</div>
+          <div class="forecast-icon">🌞</div>
+          <div class="forecast-temps">
+            <div class="forecast-temp">
+              <strong>60°</strong>
+            </div>
+            <div class="forecast-temp">39°</div>
+          </div>
+        </div>
+      `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", changeCity);
 
 searchCity("Slinger");
+displayForecast();
